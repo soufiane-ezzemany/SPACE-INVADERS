5a4227a9d104a85aea3c5e06497c427eebe80026
Soufiane Ezzemany <soufianezzemany@gmail.com>
2022-05-18 16:42:38 +0200

    Ajout de la classe SpaceInvader pour l'initialisation du jeu

---
+++ Source/Space Invaders/Space Invaders/Logic/SpaceInvader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Space_Invaders.Logic
+{
+    public class SpaceInvader : IUTGame.Game
+    {
+        public SpaceInvader(Canvas canvas) : base(canvas, "Sprites", "Sounds")
+        {
+
+        }
+        protected override void InitItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RunWhenLoose()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RunWhenWin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

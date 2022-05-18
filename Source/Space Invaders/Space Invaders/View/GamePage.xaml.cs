147cc4301f8b4a85a423f33df0147966f53d2b3b
Soufiane Ezzemany <soufianezzemany@gmail.com>
2022-05-18 16:20:04 +0200

    Ajout de La page de jeu et le style

---
+++ Source/Space Invaders/Space Invaders/View/GamePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Space_Invaders.View
+{
+    /// <summary>
+    /// Logique d'interaction pour GamePage.xaml
+    /// </summary>
+    public partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Source/Space Invaders/Space Invaders/obj/Release/netcoreapp3.1/App.g.cs
+++ Source/Space Invaders/Space Invaders/obj/Release/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace Space_Invaders {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Space_Invaders {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public static void Main() {
             Space_Invaders.App app = new Space_Invaders.App();
             app.InitializeComponent();

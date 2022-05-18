1328478bab82f60b4f57dba8a60a85268d4a3350
John GAUDRY <john.gaudry@iut-dijon.u-bourgogne.fr>
2022-05-18 19:49:12 +0200

    Creation page GameOver

---
+++ Source/Space Invaders/Space Invaders/View/GameOverPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Space_Invaders.View
+{
+    /// <summary>
+    /// Logique d'interaction pour GameOverPage.xaml
+    /// </summary>
+    public partial class GameOverPage : Page
+    {
+        public GameOverPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

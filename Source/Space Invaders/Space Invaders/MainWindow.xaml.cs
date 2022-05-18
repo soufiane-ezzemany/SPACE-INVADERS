67f193f684d1714082a0d5e190ce718c7b2d1a24
soufiane <soufianezzemany@gmail.com>
2022-05-18 15:24:00 +0200

    Ajout de font  plus les styles

--- Source/Space Invaders/Space Invaders/MainWindow.xaml.cs
+++ Source/Space Invaders/Space Invaders/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Space_Invaders.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Space_Invaders
         public MainWindow()
         {
             InitializeComponent();
+            MenuPage menu = new MenuPage();
+            myframe.Navigate(menu);
         }
     }
 }

0b27b597aa5471d2a230ff0a9a2374b5f83ce1fd
im533318 <im533318@IQ-146-11>
2022-05-19 11:51:26 +0200

    Création classe Alien Red

---
+++ Source/Space Invaders/Space Invaders/Logic/AlienRed.cs
@@ -0,0 +1,41 @@
+﻿using IUTGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Space_Invaders.Logic
+{
+    /// <summary>
+    /// Classe Alien Rouge
+    /// </summary>
+    /// <author> MESROUK Ismaïl et EZZEMANY Soufiane</author>
+    public class AlienRed : Alien
+    {
+        /// <summary>
+        /// Constructeur de Alien Red
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="canvas"></param>
+        /// <param name="game"></param>
+        /// <param name="spriteName"></param>
+        /// <author>MESROUK Ismaïl</author>
+        public AlienRed(double x, double y, Canvas canvas, Game game, string spriteName = "") : base(x, y, canvas, game, spriteName)
+        {
+        }
+
+        /// <author>MESROUK Ismaïl</author>
+        public override string TypeName => "AlienRed";
+
+        /// <summary>
+        /// Gestion des collisions
+        /// </summary>
+        /// <param name="other"></param>
+        ///<author>MESROUK Ismaïl</author>
+        public override void CollideEffect(GameItem other)
+        {
+            
+        }
+    }
+}

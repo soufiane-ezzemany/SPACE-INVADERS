dac0e45d4db579a11154420ff157e0ba4a879f1e
im533318 <im533318@IQ-146-11>
2022-05-19 10:25:10 +0200

    Ajout commentaire

--- Source/Space Invaders/Space Invaders/obj/Debug/netcoreapp3.1/View/PauseGamePage.g.i.cs
+++ Source/Space Invaders/Space Invaders/obj/Debug/netcoreapp3.1/View/PauseGamePage.g.i.cs
@@ -53,7 +53,7 @@ namespace Space_Invaders.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Space Invaders;V1.0.0.0;component/view/pausegamepage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Space Invaders;component/view/pausegamepage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\PauseGamePage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

810bd067a0734c636ffca62bd77dd10d87ba1287
JohnGdr <john.gaudry@iut-dijon.u-bourgogne>
2022-05-19 11:44:09 +0200

    Ajout de la classe abstraite Alien

---
+++ Source/Space Invaders/Space Invaders/Logic/Alien.cs
@@ -0,0 +1,29 @@
+﻿using IUTGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Space_Invaders.Logic
+{
+    /// <summary>
+    /// Classe Alien pour ennemie
+    /// </summary>
+    /// <author> John Gaudry et Soufiane Ezzemany</author>
+    abstract public class Alien : GameItem
+    {
+        /// <summary>
+        /// Constructeur Alien hérité de GameItem
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="canvas"></param>
+        /// <param name="game"></param>
+        /// <param name="spriteName"></param>
+        /// <author> Gaudry John</author>
+        protected Alien(double x, double y, Canvas canvas, Game game, string spriteName = "") : base(x, y, canvas, game, spriteName)
+        {
+
+        }
+    }
+}

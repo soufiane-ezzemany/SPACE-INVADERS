4ba482a5cd489bb8629e2f744dac3ef229dfa841
Soufiane Ezzemany <soufianezzemany@gmail.com>
2022-05-19 11:56:50 +0200

    Ajout sprites et classe Bloc

---
+++ Source/Space Invaders/Space Invaders/Logic/Bloc.cs
@@ -0,0 +1,30 @@
+﻿using IUTGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Space_Invaders.Logic
+{   
+    /// <summary>
+    /// Classe pour le bloc entre le vaisseau et les aliens
+    /// </summary>
+    /// <author>Soufiane EZZEMANY</author>
+    public class Bloc : GameItem
+    {
+        public Bloc(double x, double y, Canvas canvas, Game game) : base(x, y, canvas, game, "Blocks/block1.png")
+        {
+        }
+
+        public override string TypeName => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gére la Collision
+        /// </summary>
+        /// <author>Soufiane EZZEMANY</author>
+        public override void CollideEffect(GameItem other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

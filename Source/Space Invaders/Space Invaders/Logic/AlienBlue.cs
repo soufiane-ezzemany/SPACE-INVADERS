9c8f5d92ebf30cc57eb2bff15fbef75618858200
im533318 <im533318@IQ-146-11>
2022-05-19 12:00:25 +0200

    Création AlienBlue

---
+++ Source/Space Invaders/Space Invaders/Logic/AlienBlue.cs
@@ -0,0 +1,39 @@
+﻿using IUTGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Space_Invaders.Logic
+{
+    /// <summary>
+    /// Classe Alien Bleu
+    /// </summary>
+    /// <author> Mesrouk Ismaïl</author>
+    public class AlienBlue : Alien
+    {
+        /// <summary>
+        /// Constructeur Alien Blue
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="canvas"></param>
+        /// <param name="game"></param>
+        /// <param name="spriteName"></param>
+        public AlienBlue(double x, double y, Canvas canvas, Game game, string spriteName = "") : base(x, y, canvas, game, spriteName)
+        {
+        }
+
+        public override string TypeName => "AlienBlue";
+
+        /// <summary>
+        /// Gestion des collisions
+        /// </summary>
+        /// <param name="other"></param>
+        /// <author> Mesrouk Ismaïl</author>
+        public override void CollideEffect(GameItem other)
+        {
+
+        }
+    }
+}

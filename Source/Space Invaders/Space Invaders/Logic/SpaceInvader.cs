caa1437a0a2c9701223e6628bbf1500034844224
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2022-06-07 23:40:56 +0200

    Ajout Page Loose + view de la page quand on perd

--- Source/Space Invaders/Space Invaders/Logic/SpaceInvader.cs
+++ Source/Space Invaders/Space Invaders/Logic/SpaceInvader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Space_Invaders.View;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Controls;
@@ -66,7 +67,8 @@ namespace Space_Invaders.Logic
         /// <author> Soufiane Ezzemany </author>
         protected override void RunWhenLoose()
         {
-            System.Windows.MessageBox.Show("Tu as perdu");
+            GameLooseWindow looseWindow = new GameLooseWindow();
+            looseWindow.Show();
         }
 
         /// <summary>

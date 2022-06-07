41204215712c59f9ca3a7803a0cdf8a5bd7ea371
John Gaudry <JohnGdr>
2022-06-07 17:19:22 +0200

    Ajouts des mouvement des Aliens

--- Source/Space Invaders/Space Invaders/Logic/Alien.cs
+++ Source/Space Invaders/Space Invaders/Logic/Alien.cs
@@ -6,12 +6,14 @@ using System.Windows.Controls;
 
 namespace Space_Invaders.Logic
 {
+
     /// <summary>
     /// Classe Alien pour ennemie
     /// </summary>
     /// <author> John Gaudry et Soufiane Ezzemany</author>
-    abstract public class Alien : GameItem
+    abstract public class Alien : GameItem, IAnimable
     {
+        private double angle = 0;
         /// <summary>
         /// Constructeur Alien hérité de GameItem
         /// </summary>
@@ -25,5 +27,35 @@ namespace Space_Invaders.Logic
         {
 
         }
+
+        /// <summary>
+        /// Mouvemet des Aliens
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <author>John Gaudry</author>
+        public void Animate(TimeSpan dt)
+        {
+
+            if (Left < 0)
+            {
+                angle = 0;
+                Left = 0;
+                MoveXY(0, 10);
+                // Rebondir();
+            }
+            else if (Right > 1200)
+            {
+                angle = -180;
+                Right = 1200;
+                MoveXY(0, 10);
+            }
+
+            MoveDA(10, angle);
+
+
+
+        }
+
+
     }
 }

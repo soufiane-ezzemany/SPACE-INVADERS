a00a60662bf6858c4411cd2db83058af749c7121
Soufiane Ezzemany <soufianezzemany@gmail.com>
2022-06-08 12:59:00 +0200

    Change from pages to window (GameWindow)

---
+++ Source/Space Invaders/Space Invaders/View/GamePageWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Space_Invaders.View
+{
+    /// <summary>
+    /// Logique d'interaction pour GamePageWindow.xaml
+    /// </summary>
+    public partial class GamePageWindow : Window
+    {
+        public GamePageWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Permet l'ouverture via naviguation de la page Pause
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author> John Gaudry </author>
+        private void PauseSetting(object sender, RoutedEventArgs e)
+        {
+            //PauseGameWindow pause = new PauseGamePage();
+            //pause.Show();
+        }
+    }
+}

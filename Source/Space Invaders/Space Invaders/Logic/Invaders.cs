d82c98c4c0f7ad44497236d7ae5ac57fdfeed97d
Soufiane Ezzemany <soufianezzemany@gmail.com>
2022-06-12 18:22:53 +0200

    Ajout commentaire Invaders

--- Source/Space Invaders/Space Invaders/Logic/Invaders.cs
+++ Source/Space Invaders/Space Invaders/Logic/Invaders.cs
@@ -5,19 +5,34 @@ using System.Text;
 using System.Windows.Controls;
 
 namespace Space_Invaders.Logic
-{
+{   
+    /// <summary>
+    /// Classe contenant tous les enemis de jeueur
+    /// </summary>
+    /// <author>Soufiane EZZEMANY</author>
     public class Invaders : GameItem, IAnimable
-    {
+    {  
         private List<Alien> aliens;
         private Canvas canvas;
         private Game game;
         private bool moveRight = true;
         private TimeSpan apparitionUFO;
 
+        /// <summary>
+        /// liste des aliens dans le jeu
+        /// </summary>
         public List<Alien> Aliens { get => aliens; set => aliens = value; } 
 
         public override string TypeName => "";
-
+        /// <summary>
+        /// Constructeur de la classe Invaders permet de placer les aliens et un vesseau
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="canvas"></param>
+        /// <param name="game"></param>
+        /// <param name="spriteName"></param>
+        /// <author>Soufiane EZZEMANY</author>
         public Invaders(double x, double y, Canvas canvas, Game game, string spriteName = "") : base(x, y, canvas, game, spriteName)
         {
             this.canvas = canvas;
@@ -28,6 +43,10 @@ namespace Space_Invaders.Logic
             this.apparitionUFO = new TimeSpan(0, 0, 0, r.Next(10,25));
         }
 
+        /// <summary>
+        /// Initialise les aliens dans le jeu 
+        /// </summary>
+        /// <author>Soufiane EZZEMANY</author>
         private void InitializeAliens()
         {
             for (int i = 1; i <= 12; i++)
@@ -39,6 +58,9 @@ namespace Space_Invaders.Logic
             DrawAliens();
         }
 
+        /// <summary>
+        /// Met les aliens dans le canvas de jeu
+        /// </summary>
         private void DrawAliens()
         {
             foreach(Alien a in aliens)
@@ -47,8 +69,14 @@ namespace Space_Invaders.Logic
             }
         }
 
+        /// <summary>
+        /// Gére tous les animations des ennemies et mouvements
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <author>Soufiane EZZEMANY</author>
         public void Animate(TimeSpan dt)
-        {
+        {   
+            //je deplace chaque alien soit à gauche soit à droite
             foreach (Alien alien in aliens)
             {
                 if (moveRight)
@@ -61,20 +89,23 @@ namespace Space_Invaders.Logic
                 }
             }
 
+            //si il change de direction le bloc va en bas
             if (this.ChangeDirection())
             {
                 MoveAliensDown();
             }
 
+            //s'il arrive à un certaine niveu de jeu 
+            //ou il y a le joueur on perd
             if (this.ArrivedToEnd())
             {
                 Game.Loose();
             }
-
+            //Gerer l'apparition de l'Ufo
             apparitionUFO = apparitionUFO - dt;
             if (apparitionUFO.TotalSeconds < 0)
             {
-                // 
+                // Creation d'ufo
                 UFO ufo = new UFO(GameWidth, 80, canvas, this.Game);
                 Game.AddItem(ufo);
                 // Réinitialisation intervalle de temps
@@ -84,6 +115,11 @@ namespace Space_Invaders.Logic
             }
         }
 
+        /// <summary>
+        /// Change la direction de bloc des aliens si on arrive dans les extremité
+        /// </summary>
+        /// <returns>booléen si le bloc change direction</returns>
+        /// <author>Soufiane EZZEMANY</author>
         private bool ChangeDirection()
         {
             foreach (Alien alien in aliens)
@@ -102,6 +138,10 @@ namespace Space_Invaders.Logic
             return false;
         }
 
+        /// <summary>
+        /// Deplacer tous les alliens en bas de 3 px
+        /// </summary>
+        /// <author>Soufiane EZZEMANY</author>
         private void MoveAliensDown()
         {
             foreach (Alien alien in aliens)
@@ -110,6 +150,10 @@ namespace Space_Invaders.Logic
             }
         }
 
+        /// <summary>
+        /// Verifier si le bloc est arrivé au niveau de joueur
+        /// </summary>
+        /// <returns></returns>
         public bool ArrivedToEnd()
         {
             foreach (Alien alien in aliens)
